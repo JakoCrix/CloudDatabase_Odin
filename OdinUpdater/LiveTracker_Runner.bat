3cb19293e62d2c229215a00d42c0fa556846428f
JakoCrix <69356638+JakoCrix@users.noreply.github.com>
2021-04-29 23:51:56 +1000

    Insertion file and batch file

---
+++ OdinUpdater/LiveTracker_Runner.bat
@@ -0,0 +1,4 @@
+call C:\Users\Andrew\miniconda3\Scripts\activate.bat
+call activate EnviOdinCloud
+C:\Miniconda3\envs\EnviOdinCloud\python.exe "C:\Users\Andrew\Documents\GitHub\CloudDatabase_Odin\OdinUpdater\Insertion.py"
+pause
