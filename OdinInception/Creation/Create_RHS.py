fc92bf8c688a2650f07c1782302f89baff4d1d71
JakoCrix <69356638+JakoCrix@users.noreply.github.com>
2021-04-29 23:34:57 +1000

    Odin DB Inception Files

--- OdinInception/Creation/Create_RHS.py
+++ OdinInception/Creation/Create_RHS.py
@@ -1,6 +1,6 @@
 # %% Admin
 import pyodbc
-from Helper.Connections import connect_to_odinprod
+from Helper.Connections_Database import connect_to_odinprod
 
 conn_odin_str, conn_odin_obj= connect_to_odinprod()
 cursor= conn_odin_obj.cursor()
